ce1eb3aca394a952c6f49f2fb185748b1eb74a31
ANSK <64350016f8169dc538aa4792480992abe332db20>
2018-02-02 10:34:59 -0500

    Updating code and added more test. -Amin

--- src/main/java/StudentUtil.java
+++ src/main/java/StudentUtil.java
@@ -1,8 +1,10 @@
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 
 public class StudentUtil {
+
     /**
      * This application is written to calculate student's level based on two date inputs.
      *
@@ -12,16 +14,23 @@ public class StudentUtil {
      */
     public int getGradeOnGivenDate(String highSchoolGradDate, String givenDate) {
 
-        LocalDate graduationDt, givenD;
-
-        try {
-            givenD = LocalDate.parse(givenDate);
-            graduationDt = LocalDate.parse(highSchoolGradDate);
-        } catch (DateTimeParseException e) {
-            System.out.println("Please provide valid date!");
-            throw e;
+        LocalDate graduationDate = parse(highSchoolGradDate);
+        if (graduationDate == null) {
+            throw new IllegalArgumentException("highSchoolGradDate is not valid (ex:2017-09-22) : " + highSchoolGradDate);
         }
+        LocalDate referenceDate;
+        if (null == givenDate) {
+            referenceDate = LocalDate.now();
+        } else {
+            referenceDate = parse(givenDate);
+            if (referenceDate == null) {
+                throw new IllegalArgumentException("givenDate is not valid (ex:2017-09-22) : " + givenDate);
+            }
+        }
+        return getGrade((LocalDate) graduationDate, (LocalDate) referenceDate);
+    }
 
+    private int getGrade(LocalDate graduationDt, LocalDate givenD) {
         long days = 4380 - ChronoUnit.DAYS.between(givenD, graduationDt);
 
         if (days < -278)
@@ -36,9 +45,27 @@ public class StudentUtil {
             return 99;
     }
 
+    public LocalDate parse(String date) {
+
+        LocalDate result = null;
+        try {
+            result = LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+            throw new DateTimeException("Please provide valid date (ex:2017-09-22)!");
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         StudentUtil studentUtil = new StudentUtil();
         int grade = studentUtil.getGradeOnGivenDate("2016-06-02", "2016-06-02");
-        System.out.println(grade);
+        System.out.println("a : " + grade);
+
+        grade = studentUtil.getGradeOnGivenDate("2000-06-02", "2016-06-02");
+        System.out.println("b : " + grade);
+
+        grade = studentUtil.getGradeOnGivenDate("2016-06-02", "2016-06-03");
+        System.out.println("c : " + grade);
+
     }
 }
--- src/test/java/StudentUtilTest.java
+++ src/test/java/StudentUtilTest.java
@@ -23,15 +23,20 @@ public class StudentUtilTest {
         assertEquals(0, studentUtil.getGradeOnGivenDate("2016-06-02", "2003-09-01"));
         assertEquals(-1, studentUtil.getGradeOnGivenDate("2016-06-02", "2003-08-01"));
         assertEquals(-1, studentUtil.getGradeOnGivenDate("2016-06-02", "2002-06-01"));
+        assertEquals(99, studentUtil.getGradeOnGivenDate("2016-06-02", null));
     }
 
     @Test
     @DisplayName("Testing Exception handling")
-    public void testExceptions() {
+    public void testException() {
         Throwable exception = assertThrows(DateTimeException.class, () -> {
-            throw new DateTimeException("Please provide valid date!");
+            throw new DateTimeException("Please provide valid date (ex:2017-09-22)!");
         });
-        assertEquals("Please provide valid date!", exception.getMessage());
+        try {
+            studentUtil.parse("2315");
+        } catch (DateTimeException e){
+            assertEquals(e.getMessage(),exception.getMessage());
+        }
     }
 
 }
